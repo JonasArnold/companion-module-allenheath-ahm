3ffc516d7be1bb886c8151ead3ae952ac2a62cdc
Jonas Arnold <jonas.arnold@live.de>
2026-07-16 12:55:28 +0200

    changed manually tracked channels being stored 1-based to being stored 0-based (rationale: auto tracked channels are stored 0-based as well and method isManuallyTracked does also work with 0-based) improved logging

--- src/state/manualTrack.js
+++ src/state/manualTrack.js
@@ -1,26 +1,35 @@
 import { ChannelType } from '../utility/constants.js'
-import { parseIDsToArray } from '../utility/helpers.js'
+import { parseIDsToArray, getChannelTypeName } from '../utility/helpers.js'
 
 export function createManualTracking(state) {
 	/**
 	 * Loads manually-tracked channels into state, overwriting
-	 * @param {ChannelType} type
-	 * @param {String} ids
+	 * @param {ChannelType} type - ChannelType
+	 * @param {String} channels - comma-separated string of channel numbers (1-indexed)
 	 */
-	function setManualTracking(type, ids) {
-		if (!ids || (typeof ids !== 'string' && !Array.isArray(ids))) {
+	function setManualTracking(type, channels) {
+		// invalid parameters, clear manually tracked channels for this type
+		if (!channels || (typeof channels !== 'string' && !Array.isArray(channels))) {
 			state.manuallyTrackedChannels[type] = []
 			return
 		}
 
-		const trackingArray = parseIDsToArray(ids)
-		state.manuallyTrackedChannels[type] = trackingArray
+		// parse into number array (still 1-indexed here)
+		const trackingArray = parseIDsToArray(channels)
 		console.log(
-			`manually tracked ${type}: ${state.manuallyTrackedChannels[type]} ${typeof state.manuallyTrackedChannels[type]}`,
-		) // returns object
+			`Manually tracking ${getChannelTypeName(type)} ${trackingArray}`,
+		)
 
+		// subtract 1 from each channel number to convert to 0-indexed
+		for (let i = 0; i < trackingArray.length; i++) {
+			trackingArray[i] -= 1
+		}
+
+		// store the manually tracked channels in state
+		state.manuallyTrackedChannels[type] = trackingArray
+
+		// add channels to auto tracking
 		for (const m of trackingArray) {
-			console.log('logging m', m, typeof m) // returns string
 			state.addChannel(type, m)
 		}
 	}
